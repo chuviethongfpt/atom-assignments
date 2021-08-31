39930bba9b0e2d5019deaf4ef3f727519bc5e9b2
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-08-31 14:38:32 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -8,7 +8,7 @@ import streamlit.components.v1 as stc
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
-   st.secrets["datawarehouse"]
+   st.secrets["handpick"]
 )
 client = bigquery.Client(credentials=credentials)
 
