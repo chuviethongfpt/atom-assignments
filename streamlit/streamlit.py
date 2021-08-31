ee98663a92228f3c3010ca7393dae4ea6390060b
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-08-31 22:11:22 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -29,10 +29,8 @@ import streamlit.components.v1 as stc
 st.set_page_config(layout="wide")
 st.title("Handpick Files")
 st.sidebar.subheader("Import Master Data")
-
 #Setup file upload
 uploaded_file = st.sidebar.file_uploader(label="Upload your Excel files.")
-                                        
 #global df
 if uploaded_file is not None:
     try:
