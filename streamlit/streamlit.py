207461cd7fd124ae425a25922eced1341fa00de5
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-08-31 21:55:13 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from google.oauth2 import service_account
-from google.cloud import bigquery
+#from google.oauth2 import service_account
+#from google.cloud import bigquery
 
 import streamlit.components.v1 as stc
 
-Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-  st.secrets[""]
+#Create API client.
+#credentials = service_account.Credentials.from_service_account_info(
+  #st.secrets[""]
 )
-client = bigquery.Client(credentials=credentials)
+#client = bigquery.Client(credentials=credentials)
 
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
+#@st.cache(ttl=600)
+#def run_query(query):
+   # query_job = client.query(query)
+   # rows_raw = query_job.result()
     # Convert to list of dicts. Required for st.cache to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
+    #rows = [dict(row) for row in rows_raw]
+   # return rows
 
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+#rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
 
 # Uses st.cache to only rerun when the query changes or after 10 min.
 
