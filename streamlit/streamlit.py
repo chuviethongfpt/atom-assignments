edcaf617e00ba146d087bc6dd33424c4cd0f5c00
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-09-01 10:02:18 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,24 +1,41 @@
-import os
+import pandas as pd
+import numpy as np
 import streamlit as st
+import json
+import os
+from google.cloud import bigquery
 
+# If you don't specify credentials when constructing the client, the
+# client library will look for credentials in the environment.
+client = bigquery.Client()
 
-def st_file_selector(st_placeholder, path='.', label='Please, select a file/folder...'):
-    # get base path (directory)
-    base_path = '.' if path is None or path is '' else path
-    base_path = base_path if os.path.isdir(
-        base_path) else os.path.dirname(base_path)
-    base_path = '.' if base_path is None or base_path is '' else base_path
-    # list files in base path directory
-    files = os.listdir(base_path)
-    if base_path is not '.':
-        files.insert(0, '..')
-    files.insert(0, '.')
-    selected_file = st_placeholder.selectbox(
-        label=label, options=files, key=base_path)
-    selected_path = os.path.normpath(os.path.join(base_path, selected_file))
-    if selected_file is '.':
-        return selected_path
-    if os.path.isdir(selected_path):
-        selected_path = st_file_selector(st_placeholder=st_placeholder,
-                                         path=selected_path, label=label)
-    return selected_path
+#Streamlit app
+st.set_page_config(layout="wide")
+st.title("Handpick Files")
+
+#Function
+def file_selector(folder_path='.', target="background"):
+    filenames = [f for f in os.listdir(folder_path) if
+                 not f[0] == "."]  # get file names from dir excluding hidden files
+    selected_filename = st.selectbox(f'Select a {target}', filenames)
+    abs_path = os.path.join(folder_path, selected_filename)
+    if os.path.isdir(abs_path):
+        return file_selector(abs_path, target)
+    return os.path.join(folder_path, selected_filename)
+
+#Setup file upload
+uploaded_file = st.file_uploader(label="Import Master Data Here.")
+
+global df
+if uploaded_file is not None:
+    try:
+        df= pd.read_excel(uploaded_file)
+    except Exception as e:
+        print(e)
+        df= pd.read_csv(uploaded_file)
+try:
+    st.write(df)
+except Exception as e:
+    print(e)
+    st.write("Please upload an Excel file")
+
