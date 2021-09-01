10378ba68bd29b36a53defeb50edf358f2c1e6ba
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-09-01 09:22:06 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,29 +1,20 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from google.oauth2 import service_account
+import os
 from google.cloud import bigquery
 
-import streamlit.components.v1 as stc
+# Define credentials 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'handpick.json'
+client = bigquery.Client()
+def _fetch_data_bigquery(query):
+    """
+      Take SQL query in Standard SQL and returns a Pandas DataFrame of results
+      ref: https://cloud.google.com/bigquery/docs/reference/standard-sql/enabling-standard-sql
+    """
+    return client.query(query, location="US").to_dataframe()
 
-# Perform query.
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
-
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
-
+#Streamlit app
 st.set_page_config(layout="wide")
 st.title("Handpick Files")
 
