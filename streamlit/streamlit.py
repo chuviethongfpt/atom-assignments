f3a7f03a6bc0a3f4df91a4327c98be2e6dca3b84
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-09-02 12:01:57 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,28 +1,19 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-import json
-import os
-from google.cloud import bigquery
+import numpy as np
 from google.oauth2 import service_account
+import pandas as pd
+import pandas_gbq
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
-# Define credentials 
-client = bigquery.Client()
-
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
 #Streamlit app
 st.set_page_config(layout="wide")
 st.title("Handpick Files")
-
-#Function
-def file_selector(folder_path='.', target="background"):
-    filenames = [f for f in os.listdir(folder_path) if
-                 not f[0] == "."]  # get file names from dir excluding hidden files
-    selected_filename = st.selectbox(f'Select a {target}', filenames)
-    abs_path = os.path.join(folder_path, selected_filename)
-    if os.path.isdir(abs_path):
-        return file_selector(abs_path, target)
-    return os.path.join(folder_path, selected_filename)
 
 #Setup file upload
 uploaded_file = st.file_uploader(label="Import Master Data Here.")
@@ -40,3 +31,15 @@ except Exception as e:
     print(e)
     st.write("Please upload an Excel file")
 
+#BigQuery
+sql="SELECT * FROM customer.df1"
+df1 = pandas_gbq.read_gbq(sql, project_id="handpick-datawarehouse-hdw", credentials=credentials)
+project_id="handpick-datawarehouse-hdw"
+
+
+# #data_frame1,2,3
+
+
+table_id = uploaded_file
+
+pandas_gbq.to_gbq(df1, table_id, project_id=project_id)
