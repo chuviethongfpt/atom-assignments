9164abd6df3ea678319bb0961d474f1025c83825
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-09-16 10:06:17 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -78,11 +78,11 @@ if st.checkbox("Import Files"):
     if product_file is not None:
         print('Process: ', product_file, db_table)
         try:
-            df1= pd.read_excel(product_file)
+            df1= pd.read_excel(product_file,encoding='utf8')
             #st.write(df1)
         except Exception as e:
             print(e)
-            df1= pd.read_csv(product_file)
+            df1= pd.read_csv(product_file, encoding='utf8')
         try:
             st.write(df1)
         except Exception as e:
