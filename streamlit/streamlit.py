286b5cae4edb0362b2156eb46c248d78937e0536
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-09-16 16:02:32 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -16,7 +16,7 @@ import sys
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets[gcp_service_account]
+    st.secrets['gcp_service_account']
 )
 client = bigquery.Client(credentials=credentials)
 
