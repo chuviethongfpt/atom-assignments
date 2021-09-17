1e6fff18dbfaef61052a13c744da334c650d61be
Viet Hong Chu <chuviethong.fpt@gmail.com>
2021-09-17 11:30:08 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -86,9 +86,8 @@ def push_exit_table(df, db_table):
                 client_id= "105542999787496052690",
                 auth_uri= "https://accounts.google.com/o/oauth2/auth",
                 token_uri= "https://oauth2.googleapis.com/token",
-                auth_provider_x509_cert_url= "https://www.googleapis.com/oauth2/v1/certs",
-                client_x509_cert_url= "https://www.googleapis.com/robot/v1/metadata/x509/datawarehouse%40hp-data-324704.iam.gserviceaccount.com"
-    )
+                auth_provider_x509_cert_url= "https://www.googleapis.com/oauth2/v1/certs"
+      )
     client = bigquery.Client(credentials=credentials)
 
     project_id="hp-data-324704"
